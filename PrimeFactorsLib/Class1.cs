039f084af919f7700e2bf9d4d54f15e85cff9cac
leoharrisn <dbahenalopez@hotmail.com>
2021-03-06 11:44:05 -0600

    Created the GetPrimeFactors Mehtod

--- PrimeFactorsLib/Class1.cs
+++ PrimeFactorsLib/Class1.cs
@@ -2,7 +2,43 @@
 
 namespace PrimeFactorsLib
 {
-    public class Class1
+    public class PrimeFactors
     {
+        
+        public string GetPrimeFactors (int numberExtenal)
+        {
+
+            string factors="";
+            int numberInternal;
+            int primeNumber = 2;
+            int iterator = 0;
+            numberInternal = numberExtenal;
+
+            while (numberInternal != 1)
+            {
+                
+                if ((numberInternal % primeNumber) != 0)
+                {
+                        primeNumber++;
+                        
+                }
+                else
+                {
+
+                    numberInternal /= primeNumber;
+
+                    factors += primeNumber.ToString();
+
+                    iterator++;
+
+                }
+                
+
+            }
+            
+            
+            return factors;
+        } 
+        
     }
 }
